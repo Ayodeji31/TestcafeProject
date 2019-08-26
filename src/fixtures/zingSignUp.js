ec496da26181cab9deaae2796968d2f3e2d50302
Ayodeji31 <josephay34@gmail.com>
2019-08-26 13:16:44 +0100

    screenshot folder added

--- src/fixtures/zingSignUp.js
+++ src/fixtures/zingSignUp.js
@@ -17,27 +17,29 @@ fixture`Zing Sign Up`
 .page(config.ZING);
 
 test("Sign Up to Zing", async t => {
+ // await t.setTestSpeed(0.05)
   await signUp.signUpToZing();
+  await t.takeScreenshot();
   await signUp.verifyCustomer();
   await t.expect()
 });
 
-test("SignUp with a blank First Name", async t => {
+test.skip("SignUp with a blank First Name", async t => {
   await signUp.errorFirstname();
   await t.expect(signUp.Error_Firstname.visible).ok();
 });
 
-test("SignUp with a blank Last Name", async t => {
+test.skip("SignUp with a blank Last Name", async t => {
   await signUp.errorLastname();
   await t.expect(signUp.Error_Lastname.visible).ok();
 });
 
-test("SignUp with a blank Email Address", async t => {
+test.skip("SignUp with a blank Email Address", async t => {
   await signUp.errorEmailAddress();
   await t.expect(signUp.Error_EmailAddress.visible).ok();
 });
 
-test("SignUp with a blank Password", async t => {
+test.skip("SignUp with a blank Password", async t => {
   await signUp.errorPassword();
   await t.expect(signUp.Error_EmailAddress.visible).ok();
 });
